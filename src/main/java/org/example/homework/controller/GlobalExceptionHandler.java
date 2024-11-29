f49a37da82e2591f6da7b07c24236b9a6a20275c
Bhadra2811 <bhadranair2811@gmail.com>
2024-11-29 17:55:50 +0530

    Implement GET API and exception handling for Property

--- src/main/java/org/example/homework/controller/GlobalExceptionHandler.java
+++ src/main/java/org/example/homework/controller/GlobalExceptionHandler.java
@@ -1,4 +1,16 @@
 package org.example.homework.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(PropertyNotFoundException.class)
+    public ResponseEntity<String> handlePropertyNotFound(PropertyNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
 }
+
